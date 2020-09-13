2a01ec56fbea26d4cc83577301121776cc132e5e
Acter <xncj01@126.com>
2020-09-13 23:15:54 +0800

    first submit

---
+++ Week_06/647.回文子串.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=647 lang=python3
+#
+# [647] 回文子串
+#
+
+# @lc code=start
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        count = 0
+        for j in range(n):
+            for i in range(0, j + 1):
+                length = j - i + 1
+                if length == 1:
+                    dp[i][j] = True
+                    count += 1
+                if length == 2 and s[i] == s[j]:
+                    dp[i][j] = True
+                    count += 1
+                if length > 2 and s[i] == s[j] and dp[i+1][j-1] is True:
+                    dp[i][j] = True
+                    count += 1
+        return count
+        
+# @lc code=end
+
